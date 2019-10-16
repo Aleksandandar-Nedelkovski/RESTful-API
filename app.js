374261f2b33af565ac1995e4e9ccafbde64c3826
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-16 16:18:40 -0500

    create, read, update, patch, and delete for specific articles

--- app.js
+++ app.js
@@ -68,16 +68,56 @@ app.route("/articles")
     );
 
 //Specific Article
-app.route("/article/:articleTitle")
+app.route("/articles/:articleTitle")
 
     .get(function (req, res) {
-        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticles) {
-            if (foundArticles) {
-                res.send(foundArticles);
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
             } else {
-                res.send(err)
+                res.send("No articles matching that title was found.");
             }
         });
+    })
+
+    .put(function (req, res) {
+        Article.update(
+            { title: req.params.articleTitle },
+            { title: req.body.title, content: req.body.content },
+            { overwrite: true },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated the selected article.");
+                }
+            }
+        );
+    })
+
+    .patch(function (req, res) {
+        Article.update(
+            { title: req.params.articleTitle },
+            { $set: req.body },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully updated article.");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
+    })
+
+    .delete(function (req, res) {
+        Article.deleteOne(
+            { title: req.params.articleTitle },
+            function (err) {
+                if (!err) {
+                    res.send("Successfully deleted the corresponding article.");
+                } else {
+                    res.send(err);
+                }
+            }
+        );
     });
 
 app.listen(6969, function () {
