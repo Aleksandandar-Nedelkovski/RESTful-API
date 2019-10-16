45953ea285f5c77ce25c43dee777de91c99b6e66
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-16 12:51:55 -0500

    chain method for /article route

---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+//TODO
+
+app.get("/", function (req, res) {
+    res.send("Success");
+})
+
+// All Articles
+app.route("/articles")
+    .get(
+        function (req, res) {
+            Article.find({}, function (err, foundArticles) {
+                if (!err) {
+                    res.send(foundArticles);
+                } else {
+                    res.send(err)
+                }
+            })
+        })
+
+    .post(
+        function (req, res) {
+            const newArticle = new Article({
+                title: req.body.title,
+                content: req.body.content
+            })
+            newArticle.save(function (err) {
+                if (!err) {
+                    res.send("Successfully added a new article");
+                } else {
+                    res.send(err);
+                }
+            });
+        }
+    )
+
+    .delete(
+        function (req, res) {
+            Article.deleteMany({}, function (err) {
+                if (!err) {
+                    res.send("Successfully deleted all article");
+                } else {
+                    res.send(err);
+                }
+            })
+        }
+    );
+
+//Specific Article
+app.route("/article/:articleTitle")
+
+    .get(function (req, res) {
+        Article.findOne({ title: req.params.articleTitle }, function (err, foundArticles) {
+            if (foundArticles) {
+                res.send(foundArticles);
+            } else {
+                res.send(err)
+            }
+        });
+    });
+
+app.listen(6969, function () {
+    console.log("Server started on port 3000");
+});
